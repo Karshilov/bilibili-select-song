5dc64f0d60ab2e00518bc07a62f5418c1c58937c
Karshilov <karshilov@foxmail.com>
2021-05-15 03:27:41 +0800

    remove sth.

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,6 @@ import icon from '../assets/icon.svg';
 import AuthGuard from './component/AuthGuard';
 import './style/tailwind.global.css';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
-
 const APP: React.FC = () => (
   <Provider store={store}>
     <AuthGuard>
